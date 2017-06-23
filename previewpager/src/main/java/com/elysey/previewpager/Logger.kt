ac2a3b3269aae4842405fd503f974539f2336cf9
Elysey <elyseyvova@gmail.com>
2017-06-23 16:30:54 +0300

    init commit

---
+++ previewpager/src/main/java/com/elysey/previewpager/Logger.kt
@@ -0,0 +1,14 @@
+package com.elysey.previewpager
+
+import android.util.Log.d
+
+/**
+ * Created by Elysey on 23.06.17.
+ */
+
+object Logger {
+
+    fun loger(text: String) {
+        d("TAG", text)
+    }
+}
